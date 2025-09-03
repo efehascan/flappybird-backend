11585a6bdebbdfffef5f6158d0a00a60a4dccb51
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-09-03 18:13:12 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,24 @@
-import express from "express";
+    // src/server.ts
+    import express from "express";
+    import dotenv from "dotenv";
+    import mySQLUtils from "./utils/MySQLUtils.js";
 
+    dotenv.config();
+
+    const app = express();
+    const PORT = Number(process.env.PORT) || 3000;
+
+    app.use(express.json());
+
+    // basit kontrol endpointi
+    app.get("/health", async (_req, res) => {
+        const dbOk = await mySQLUtils.testConnection();
+        res.json({ ok: true, db: dbOk ? "connected" : "failed" });
+    });
+
+
+    // Routes
+
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
